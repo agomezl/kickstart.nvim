39fc06c5a8103c6e51b3c7d2f99fffd9efdca941
Alejandro Gómez-Londoño <alejandro.gomez-londono@zenseact.com>
2023-08-23 15:21:46 +0200

    Add cutom keybingins file

--- lua/agomezl/init.lua
+++ lua/agomezl/init.lua
@@ -3,5 +3,5 @@ vim.api.nvim_create_augroup('agomezl',{})
 require('agomezl.spellcheck')
 require('agomezl.terminal')
 require('agomezl.whitespaces')
-
+require('agomezl.keys')
 
---
+++ lua/agomezl/keys.lua
@@ -0,0 +1,5 @@
+
+-- [[Personal keymaps]]
+vim.keymap.set('n',',p','o<ESC>p',{})
+vim.keymap.set('n',',P','O<ESC>p',{})
+
