a7a068ea8661683b0c5f5777b5aa7bf98f990913
Alejandro Gómez-Londoño <alejandro.gomez-londono@zenseact.com>
2023-08-23 09:21:35 +0200

    Add a directory tree to nvim

---
+++ lua/custom/plugins/tree.lua
@@ -0,0 +1,29 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+end
+
+
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  config = function()
+    require('nvim-tree').setup()
+    vim.keymap.set('n', '<leader>t', ':NvimTreeFindFileToggle<CR>',
+      { desc = "nvim-tree: Toggle", noremap = true, silent = true, nowait = true }
+    )
+  end,
+}
