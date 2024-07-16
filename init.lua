160161f9f998f783901d19d2acdf8c32631fea2d
Alejandro Gómez-Londoño <alejandro.gomez-londono@zenseact.com>
2024-07-16 14:56:39 +0000

    Add formatter support

--- init.lua
+++ init.lua
@@ -554,7 +554,7 @@ local on_attach = function(_, bufnr)
   end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+  vim.api.nvim_buf_create_user_command(bufnr, 'LSPFormat', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
 end
