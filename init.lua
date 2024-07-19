4c981cd345489a039b59918acbbfa60682759ba9
Alejandro Gómez-Londoño <alejandro.gomez-londono@zenseact.com>
2024-07-19 13:03:49 +0000

    Add harper as spell checker!

--- init.lua
+++ init.lua
@@ -568,7 +568,7 @@ require('lazy').setup({
         -- gopls = {},
         pyright = {},
         bashls = {},
-        harper-ls = {},
+        harper_ls = {},
 
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
